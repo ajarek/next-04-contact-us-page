5a8ec1fffcf016a13a958ea4c6e6cfcd4d4ac4c3
Ajarek <ajarek@poczta.onet.pl>
2023-10-02 23:07:24 +0200

    Initial commit from Create Next App

---
+++ app/layout.js
@@ -0,0 +1,17 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Strona kontaktowa',
+  description: 'Strona kontaktowa z bazą danych mongodb',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pl">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}

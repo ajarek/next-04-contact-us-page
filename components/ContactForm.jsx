6a56a7a5ec929d650d0edb04982540b3aada4e35
Ajarek <ajarek@poczta.onet.pl>
2023-10-03 20:54:18 +0200

    added logic to the form

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 
 const ContactForm = () => {
+  const [fullName, setFullName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+ const handleSubmit=(e)=>{
+  e.preventDefault()
+  console.log('nazwisko',fullName);
+  console.log('email',email);
+  console.log('wiadomość',message);
+ }
+
   return (
     <>
-      <form action='' className='py-4 mt-4 border-t flex flex-col gap-5'>
+      <form
+        action=''
+        className='py-4 mt-4 border-t flex flex-col gap-5'
+        onSubmit={handleSubmit}
+      >
         <div>
           <label htmlFor='name'>Imie i Nazwisko</label>
           <input
             type='text'
+            onChange={(e)=>setFullName(e.target.value)}
+            value={fullName}
             placeholder='Jan Nowak'
             id='name'
           />
@@ -16,6 +35,8 @@ const ContactForm = () => {
           <label htmlFor='email'>Email</label>
           <input
             type='email'
+            onChange={(e)=>setEmail(e.target.value)}
+            value={email}
             placeholder='jan@wp.pl'
             id='email'
           />
@@ -25,17 +46,21 @@ const ContactForm = () => {
           <textarea
             name=''
             id='message'
+            onChange={(e)=>setMessage(e.target.value)}
+            value={message}
             className='h-32'
             placeholder='Tutaj wpisz swoją wiadomość...'
           ></textarea>
         </div>
         <div>
-          <button className='bg-green-700 p-3 text-white font-bold'>Wyślij</button>
+          <button className='bg-green-700 p-3 text-white font-bold'>
+            Wyślij
+          </button>
         </div>
       </form>
-     <div className='bg-slate-100 flex flex-col'>
-     <div className='text-red-600 px-5 py-2'>Error message</div>
-     </div>
+      <div className='bg-slate-100 flex flex-col'>
+        <div className='text-red-600 px-5 py-2'>Error message</div>
+      </div>
     </>
   )
 }

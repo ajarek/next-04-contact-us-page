5d96431fe12c8aa6628fb9fe901e0ba13557f353
Ajarek <ajarek@poczta.onet.pl>
2023-10-04 22:03:15 +0200

    loading & error handling added

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -3,17 +3,22 @@
 import React, { useState } from 'react'
 
 const ContactForm = () => {
-  const [error, setError] = useState([])
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
 
   const handleSubmit = async (e) => {
     e.preventDefault()
+    setIsLoading(true)
+    setError(null)//Usuwa poprzednie błędy, gdy rozpocznie się nowe żądanie
+
     const fullName = e.target[0].value
     const email = e.target[1].value
     const message = e.target[2].value
-   
+    
+
 
     try {
-      await fetch('/api/contact', {
+     const res= await fetch('/api/contact', {
         method: 'POST',
         body: JSON.stringify({
           fullName,
@@ -21,15 +26,24 @@ const ContactForm = () => {
           message,
         }),
       })
-
+      if (!res.ok) {
+        throw new Error('Nie udało się przesłać danych. Proszę spróbuj ponownie.')
+      }
+      console.log(res);
       e.target.reset()
+      
     } catch (err) {
+      setError(err.message)
       console.log(err)
+    }finally {
+      setIsLoading(false) 
     }
+    
   }
 
   return (
     <>
+      {error && <div style={{ color: 'red' }}>{error}</div>}
       <form
         className='py-4 mt-4 border-t flex flex-col gap-5'
         onSubmit={handleSubmit}
@@ -63,14 +77,12 @@ const ContactForm = () => {
           ></textarea>
         </div>
         <div>
-          <button className='bg-green-700 p-3 text-white font-bold'>
-            Wyślij
+          <button className='bg-green-700 p-3 text-white font-bold' disabled={isLoading}>
+          {isLoading ? 'Ładowanie...' : 'Wyślij'}
           </button>
         </div>
       </form>
-      <div className='bg-slate-100 flex flex-col'>
-        <div className='text-red-600 px-5 py-2'>Error message</div>
-      </div>
+      
     </>
   )
 }

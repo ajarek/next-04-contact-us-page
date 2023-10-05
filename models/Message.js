59a267e6836f3d71e6f0913155af2bcf9c214d21
Ajarek <ajarek@poczta.onet.pl>
2023-10-05 20:50:10 +0200

    added models

---
+++ models/Message.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const messageSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+   
+  },
+  { timestamps: true }
+);
+
+//Jeśli kolekcja Message nie istnieje, utwórz nową.
+export default mongoose.models.Message || mongoose.model("Message", messageSchema);
